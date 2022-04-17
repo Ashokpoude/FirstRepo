cd9613d9c86bbcbd93401fa9e9ff5d8b2bb7fba2
Ashok Poudel <ashokpoudel34@gmail.com>
2022-04-17 16:21:43 +0545

    exe file added

--- even_num.c
+++ even_num.c
@@ -2,14 +2,14 @@
 #include<conio.h>
 
 int main(){
-int n,num;
-printf("input enter number of even number you want: ");
+int n,num,i;
+printf("input enter number of even number you want upto : ");
 scanf("%d",&n);
 printf("Even numbers are : ");
 for(i=1;i<=n;i++){
 num = i  % 2;
-if (num = 0){
-printf("%d",i);
+if (num == 0){
+printf("%d  ",i);
 }
 }
 return 0;
